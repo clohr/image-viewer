9eeedc3386ecb60c3eae25b3bcb83ed9911b2094
Christian Lohr <christianlohr@gmail.com>
2018-04-16 22:49:51 -0400

    Added transformOrigin

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (Html, div)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, style)
 import Json.Decode as Decode exposing (int, string, Decoder)
 import Json.Decode.Pipeline exposing (decode, optional)
 import MouseEvents exposing (onMouseMove, relPos, MouseEvent, Position)
@@ -57,12 +57,15 @@ flagDecoder =
 init : Decode.Value -> ( Model, Cmd Msg )
 init flags =
     let
+        decodedValue : Result String Photo
         decodedValue =
             Decode.decodeValue flagDecoder flags
 
+        lastPos : MouseEvent
         lastPos =
             { clientPos = Position 0 0, targetPos = Position 0 0 }
 
+        initialModel : Model
         initialModel =
             case decodedValue of
                 Ok model ->
@@ -72,6 +75,67 @@ init flags =
                     Model (Photo "" 1920 1200 "" 640 400) lastPos
     in
         ( initialModel, Cmd.none )
+
+
+
+-- VIEW
+
+
+createTransformOrigin : MouseEvent -> Photo -> String
+createTransformOrigin evt photo =
+    let
+        percentage : Float
+        percentage =
+            photo.imgWidth // photo.viewerWidth * 100 |> toFloat
+
+        offsetX : Float
+        offsetX =
+            evt.clientPos.x - evt.targetPos.x |> toFloat
+
+        offsetY : Float
+        offsetY =
+            evt.clientPos.y - evt.targetPos.y |> toFloat
+
+        x : String
+        x =
+            offsetX / toFloat photo.imgWidth * percentage |> toString
+
+        y : String
+        y =
+            offsetY / toFloat photo.imgHeight * percentage |> toString
+    in
+        x ++ "% " ++ y ++ "%"
+
+
+createBackgroundImage : Photo -> String
+createBackgroundImage photo =
+    "url(" ++ photo.imgSrc ++ ")"
+
+
+createViewerProp : Int -> String
+createViewerProp int =
+    (toString int) ++ "px"
+
+
+view : Model -> Html Msg
+view model =
+    div
+        [ class "photo-wrapper"
+        , style
+            [ ( "width", createViewerProp model.photo.viewerWidth )
+            , ( "height", createViewerProp model.photo.viewerHeight )
+            ]
+        ]
+        [ div
+            [ class "photo"
+            , onMouseMove MouseMove
+            , style
+                [ ( "transformOrigin", createTransformOrigin model.lastPos model.photo )
+                , ( "backgroundImage", createBackgroundImage model.photo )
+                ]
+            ]
+            []
+        ]
 
 
 
@@ -85,8 +149,8 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        MouseMove ev ->
-            ( { model | lastPos = ev }, Cmd.none )
+        MouseMove evt ->
+            ( { model | lastPos = evt }, Cmd.none )
 
 
 
@@ -96,16 +160,3 @@ update msg model =
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "photo-wrapper" ]
-        [ div
-            [ class "photo", onMouseMove MouseMove ]
-            []
-        ]
