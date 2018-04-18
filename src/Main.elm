fc32e89241ea6913095ac11bc3f1203c4c94ce1b
Christian Lohr <christianlohr@gmail.com>
2018-04-17 21:52:55 -0400

    Inlined styles

--- src/Main.elm
+++ src/Main.elm
@@ -4,7 +4,7 @@ import Html exposing (Html, div)
 import Html.Attributes exposing (class, style)
 import Json.Decode as Decode exposing (int, string, Decoder)
 import Json.Decode.Pipeline exposing (decode, optional)
-import MouseEvents exposing (onMouseMove, relPos, MouseEvent, Position)
+import MouseEvents exposing (onMouseMove, onMouseOver, onMouseOut, relPos, MouseEvent, Position)
 
 
 {-| Because Flags can be optional, we want to update the signature to use Decode.Value vs. Flags so it can be decoded
@@ -26,6 +26,7 @@ main =
 type alias Model =
     { photo : Photo
     , lastPos : MouseEvent
+    , zoom : Zoom
     }
 
 
@@ -37,7 +38,6 @@ type alias Photo =
     { imgSrc : String
     , imgWidth : Int
     , imgHeight : Int
-    , imgAlt : String
     , viewerWidth : Int
     , viewerHeight : Int
     }
@@ -49,7 +49,6 @@ flagDecoder =
         |> optional "imgSrc" string ""
         |> optional "imgWidth" int 1920
         |> optional "imgHeight" int 1200
-        |> optional "imgAlt" string ""
         |> optional "viewerWidth" int 640
         |> optional "viewerHeight" int 400
 
@@ -69,10 +68,10 @@ init flags =
         initialModel =
             case decodedValue of
                 Ok model ->
-                    { photo = model, lastPos = lastPos }
+                    { photo = model, lastPos = lastPos, zoom = ZoomOut }
 
                 Err _ ->
-                    Model (Photo "" 1920 1200 "" 640 400) lastPos
+                    Model (Photo "" 1920 1200 640 400) lastPos ZoomOut
     in
         ( initialModel, Cmd.none )
 
@@ -108,8 +107,8 @@ createTransformOrigin evt photo =
 
 
 createBackgroundImage : Photo -> String
-createBackgroundImage photo =
-    "url(" ++ photo.imgSrc ++ ")"
+createBackgroundImage { imgSrc } =
+    "url(" ++ imgSrc ++ ")"
 
 
 createViewerProp : Int -> String
@@ -117,33 +116,88 @@ createViewerProp int =
     (toString int) ++ "px"
 
 
+createBaseStyles : Photo -> MouseEvent -> List ( String, String )
+createBaseStyles photo evt =
+    [ ( "backgroundImage", createBackgroundImage photo )
+    , ( "backgroundPosition", "center" )
+    , ( "backgroundRepeat", "no-repeat" )
+    , ( "backgroundSize", "cover" )
+    , ( "height", "100%" )
+    , ( "left", "0" )
+    , ( "position", "absolute" )
+    , ( "top", "0" )
+    , ( "transformOrigin", createTransformOrigin evt photo )
+    , ( "transition", "transform .3s ease-in-out" )
+    , ( "width", "100%" )
+    ]
+
+
+transformScale : Photo -> String
+transformScale { imgWidth, viewerWidth } =
+    imgWidth // viewerWidth |> toString
+
+
+createZoomStyles : Zoom -> Photo -> List ( String, String )
+createZoomStyles zoom photo =
+    case zoom of
+        ZoomIn ->
+            [ ( "cursor", "zoom-in" )
+            , ( "transform", "scale(" ++ transformScale photo ++ ".0)" )
+            ]
+
+        ZoomOut ->
+            [ ( "cursor", "pointer" )
+            , ( "transform", "scale(1.0)" )
+            ]
+
+
+createPhoto : Model -> Html Msg
+createPhoto { photo, lastPos, zoom } =
+    let
+        baseCss =
+            createBaseStyles photo lastPos
+
+        zoomCss =
+            createZoomStyles zoom photo
+    in
+        div
+            [ class "photo"
+            , onMouseMove MouseMove
+            , onMouseOver MouseOver
+            , onMouseOut MouseOut
+            , style (List.append baseCss zoomCss)
+            ]
+            []
+
+
 view : Model -> Html Msg
 view model =
     div
         [ class "photo-wrapper"
         , style
-            [ ( "width", createViewerProp model.photo.viewerWidth )
+            [ ( "box-sizing", "border-box" )
             , ( "height", createViewerProp model.photo.viewerHeight )
+            , ( "overflow", "hidden" )
+            , ( "position", "relative" )
+            , ( "width", createViewerProp model.photo.viewerWidth )
             ]
         ]
-        [ div
-            [ class "photo"
-            , onMouseMove MouseMove
-            , style
-                [ ( "transformOrigin", createTransformOrigin model.lastPos model.photo )
-                , ( "backgroundImage", createBackgroundImage model.photo )
-                ]
-            ]
-            []
-        ]
+        [ createPhoto model ]
 
 
 
 -- UPDATE
 
 
+type Zoom
+    = ZoomIn
+    | ZoomOut
+
+
 type Msg
     = MouseMove MouseEvent
+    | MouseOver MouseEvent
+    | MouseOut MouseEvent
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -151,6 +205,12 @@ update msg model =
     case msg of
         MouseMove evt ->
             ( { model | lastPos = evt }, Cmd.none )
+
+        MouseOver evt ->
+            ( { model | zoom = ZoomIn }, Cmd.none )
+
+        MouseOut evt ->
+            ( { model | zoom = ZoomOut }, Cmd.none )
 
 
 
